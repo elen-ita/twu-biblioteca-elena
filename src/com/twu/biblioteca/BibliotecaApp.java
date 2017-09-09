d2adc8f731e0cba1c54ad40f77a15ba8de956dd0
Elena <eraffay@gmail.com>
2017-09-09 17:19:51 +0200

    Added print book list

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,22 +1,57 @@
 package com.twu.biblioteca;
 
+import java.util.*;
+
 public class BibliotecaApp {
+
+    static final List<String> books = new ArrayList<String>()
+    {{
+        add("The Great Gatsby");
+        add("The Grapes of Wrath");
+        add("Nineteen Eighty-Four");
+        add("Ulysses");
+        add("Lolita");
+        add("Catch-22");
+        add("The Catcher in the Rye");
+        add("Beloved");
+        add("To Kill a Mockingbird");
+        add("One Hundred Years of Solitude");
+    }};
 
     public static void main(String[] args) {
 
-        System.out.println(welcomeMessage());
-        System.out.println();
-        System.out.println(mainMenu());
+        System.out.println(welcomeMessage() + "\n");
+
+        mainMenu();
 
     }
-
 
 
     public static String welcomeMessage() {
         return "Hello, Welcome to Biblioteca!";
     }
 
-    public static String mainMenu() {
+    public static void mainMenu() {
+        System.out.println(displayMainMenu());
+        decideMainMenuAction(readUserChoice());
+    }
+
+    private static void decideMainMenuAction(int userChoice) {
+        printBookList();
+    }
+
+    public static void printBookList() {
+        for (String book : books){
+                 System.out.println(book);
+        }
+    }
+
+    private static int readUserChoice() {
+        Scanner reader = new Scanner(System.in);
+        return reader.nextInt();
+    }
+
+    public static String displayMainMenu() {
         return "Option 1: List Books";
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -9,11 +9,14 @@ public class ExampleTest {
 
     @Test
     public void testWelcomeMessage() {
-        assertEquals("Hello, Welcome to Biblioteca!", BibliotecaApp.welcomeMessage());
+        assertEquals("Hello, Welcome to Biblioteca!", com.twu.biblioteca.BibliotecaApp.welcomeMessage());
     }
 
     @Test
     public void testMainMenu() {
-        assertEquals("Option 1: List Books", BibliotecaApp.mainMenu());
+        assertEquals("Option 1: List Books", com.twu.biblioteca.BibliotecaApp.displayMainMenu());
     }
+
+    //Does every method have to return something?
+    //I'm Unsure of how to test void methods or methods that require user input
 }
